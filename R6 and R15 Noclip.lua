48cf5df6e521b819e2315c1bc67b3f8b8ab35a55
CORYU <77679412+Not-Kyle@users.noreply.github.com>
2023-10-08 07:41:50 -0700

    Update R6 and R15 Noclip.lua

--- R6 and R15 Noclip.lua
+++ R6 and R15 Noclip.lua
@@ -1,49 +1,45 @@
 local Command = {}
-local Hotkeys = {Noclip = 'X', NoNoclip = 'C'}
+local Hotkeys = {Noclip = 'X'}
 
 getgenv().Players = game:GetService'Players'
 getgenv().Local = Players.LocalPlayer
 getgenv().UserInputService = game:GetService'UserInputService'
 getgenv().UserNotifications = function(title, text) game:GetService'StarterGui':SetCore("SendNotification", {Title = title; Text = text;}) end
+getgenv().Noclip = true
 
 function Command:Noclip()
-    if Local and Local.Character and Local.Character:FindFirstChild'Torso' then
-        Local.Character:FindFirstChild'Head'.CanCollide = false
-        Local.Character:FindFirstChild'Torso'.CanCollide = false
-    elseif Local and Local.Character and Local.Character:FindFirstChild'UpperTorso' then
-        Local.Character:FindFirstChild'Head'.CanCollide = false
-        Local.Character:FindFirstChild'LowerTorso'.CanCollide = false
-        Local.Character:FindFirstChild'UpperTorso'.CanCollide = false
-        Local.Character:FindFirstChild'HumanoidRootPart'.CanCollide = false
-    end
-    UserNotifications('Noclip', 'Noclip is now true')
-end
-
-function Command:KillNoclip()
-    if Local and Local.Character and Local.Character:FindFirstChild'Torso' then
-        Local.Character:FindFirstChild'Head'.CanCollide = true
-        Local.Character:FindFirstChild'Torso'.CanCollide = true
-    elseif Local and Local.Character and Local.Character:FindFirstChild'UpperTorso' then
-        Local.Character:FindFirstChild'Head'.CanCollide = true
-        Local.Character:FindFirstChild'LowerTorso'.CanCollide = true
-        Local.Character:FindFirstChild'UpperTorso'.CanCollide = true
-        Local.Character:FindFirstChild'HumanoidRootPart'.CanCollide = true
-    end
-    UserNotifications('Noclip', 'Noclip is now false')
+	if Noclip then
+	    if Local and Local.Character and Local.Character:FindFirstChild'UpperTorso' or Local.Character:FindFirstChild'Humanoid'.RigType == Enum.HumanoidRigType.R15 then
+	        Local.Character:FindFirstChild'Head'.CanCollide = false
+	        Local.Character:FindFirstChild'LowerTorso'.CanCollide = false
+	        Local.Character:FindFirstChild'UpperTorso'.CanCollide = false
+	        Local.Character:FindFirstChild'HumanoidRootPart'.CanCollide = false
+	    elseif Local and Local.Character and Local.Character:FindFirstChild'Torso' then
+	        Local.Character:FindFirstChild'Head'.CanCollide = false
+	        Local.Character:FindFirstChild'Torso'.CanCollide = false
+	    end
+	elseif not Noclip then
+		if Local and Local.Character and Local.Character:FindFirstChild'UpperTorso' or Local.Character:FindFirstChild'Humanoid'.RigType == Enum.HumanoidRigType.R15 then
+	        Local.Character:FindFirstChild'Head'.CanCollide = true
+	        Local.Character:FindFirstChild'LowerTorso'.CanCollide = true
+	        Local.Character:FindFirstChild'UpperTorso'.CanCollide = true
+	        Local.Character:FindFirstChild'HumanoidRootPart'.CanCollide = true
+		elseif Local and Local.Character and Local.Character:FindFirstChild'Torso' then
+	        Local.Character:FindFirstChild'Head'.CanCollide = true
+	        Local.Character:FindFirstChild'Torso'.CanCollide = true
+	    end
+	end
 end
 
 UserInputService.InputBegan:Connect(function(Array, Kill)
     if Kill then
-        retrun nil
+        return nil
     end
-    if Array.KeyCode == Enum.KeyCode[Noclip] then
+    if Array.KeyCode == Enum.KeyCode[Hotkeys.Noclip] then
+        Noclip = not Noclip
         Command:Noclip()
-    end
-    if Array.KeyCode == Enum.KeyCode[NoNoclip] then
-        Command:KillNoclip()
+        UserNotifications('Noclip', 'Noclip is now '..tostring(Noclip))
     end
 end)
 
-UserNotifications('Noclip', 'Noclip executed - By Coryu')
-
-return Command
+UserNotifications('Coryu Noclip', 'Coryus Universial noclip has been executed')
