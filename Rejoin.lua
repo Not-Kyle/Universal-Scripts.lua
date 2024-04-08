ce5c46a9194e9e1b05e630b3aa8e35aac77c8b7e
kori <77679412+Not-Kyle@users.noreply.github.com>
2024-04-08 16:18:59 -0400

    Update Rejoin.lua

--- Rejoin.lua
+++ Rejoin.lua
@@ -1,4 +1,4 @@
-getgenv().TeleportService = game:GetService'TeleprotService'
+getgenv().TeleportService = game:GetService'TeleportService'
 getgenv().Host = game:GetService'Players'.LocalPlayer
 
 TeleportService:Teleport(game.PlaceId, Host)
