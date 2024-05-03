34899efcc100f2cdb3ecf0de82aaf44c0d9246f6
kori <161272465+hellokittysouljia@users.noreply.github.com>
2024-05-03 16:00:39 -0400

    Update Audio Player.lua

--- Audio Player.lua
+++ Audio Player.lua
@@ -1,4 +1,4 @@
 local Sound = Instance.new('Sound')
 Sound.Parent = game:GetService('Workspace')
-Sound.SoundId = '' -- put the audio ID between the ''
+Sound.SoundId = '' -- put the audio ID between the apostrophes
 Sound.Playing = true
