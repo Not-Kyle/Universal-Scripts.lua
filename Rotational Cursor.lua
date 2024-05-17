1b5aee1149469a0e47cee6af8fa01cb4aab33251
kori <77679412+Not-Kyle@users.noreply.github.com>
2024-05-16 22:05:17 -0400

    Create Rotational Cursor.lua

---
+++ Rotational Cursor.lua
@@ -0,0 +1,53 @@
+local Core = game:GetService'CoreGui'
+local Host = game:GetService'Players'.LocalPlayer
+local RunService = game:GetService'RunService'
+local UserInputService = game:GetService'UserInputService'
+local Workspace = game:GetService'Workspace'
+local TweenService = game:GetService'TweenService'
+
+local Sizeable = UDim2.new(0.1,0,0.1,0)
+
+local CoryuCustomMouse = Instance.new("ScreenGui", Core)
+CoryuCustomMouse.Name = "CoryuCustomMouse"
+CoryuCustomMouse.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+CoryuCustomMouse.ResetOnSpawn = false
+
+local CursorImage = Instance.new("ImageLabel", CoryuCustomMouse)
+CursorImage.Name = "CursorImage"
+CursorImage.AnchorPoint = Vector2.new(0.5, 0.5)
+CursorImage.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+CursorImage.BackgroundTransparency = 1.000
+CursorImage.BorderColor3 = Color3.fromRGB(0, 0, 0)
+CursorImage.BorderSizePixel = 0
+CursorImage.Size = UDim2.new(0,75,0,75)
+CursorImage.Visible = true
+CursorImage.Image = "rbxassetid://316279304"
+CursorImage.ImageColor3 = Color3.fromRGB(255,85,255)
+CursorImage.ImageTransparency = 0.020
+CursorImage.ZIndex = 2e9
+
+local Circle = Drawing.new('Circle')
+Circle.Visible = true
+Circle.Radius = 75
+Circle.Color = Color3.fromRGB(255,85,255)
+Circle.Thickness = 2
+Circle.Filled = false
+Circle.Transparency = 1
+
+local Ratio = Instance.new("UIAspectRatioConstraint", CursorImage)
+
+RunService.Stepped:Connect(function()
+	Circle.Position = Vector2.new(UserInputService:GetMouseLocation().X, UserInputService:GetMouseLocation().Y)
+    CursorImage.Position = UDim2.new(0,UserInputService:GetMouseLocation().X,0, UserInputService:GetMouseLocation().Y-35)
+end)
+
+local Info = TweenInfo.new(2.5,Enum.EasingStyle.Linear,Enum.EasingDirection.In,-1,false,0)
+local Tween = TweenService:Create(CursorImage,Info, {Rotation = 360})
+
+local InfoSize = TweenInfo.new(0.85,Enum.EasingStyle.Linear,Enum.EasingDirection.InOut,-1,true,0.8)
+local TweenSize = TweenService:Create(CursorImage,InfoSize, {Size = Sizeable})
+
+Tween:Play()
+TweenSize:Play()
+
+UserInputService.MouseIconEnabled = false
