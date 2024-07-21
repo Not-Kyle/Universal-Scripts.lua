3268523f64ca66bacfb2165a25985d2d5af28b05
kori <77679412+Not-Kyle@users.noreply.github.com>
2024-07-21 08:26:31 -0700

    Update Audio Player.lua

--- Audio Player.lua
+++ Audio Player.lua
@@ -1,4 +1,5 @@
 local Sound = Instance.new('Sound')
 Sound.Parent = game:GetService('Workspace')
 Sound.SoundId = '' -- put the audio ID between the apostrophes
-Sound.Playing = true
+Sound.Playing = true;
+Sound.Looped = false; --Make true if you want it to play constantly
