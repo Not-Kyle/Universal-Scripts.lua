24a796a2fe354c97152f41bf37a416ab43172369
kori <77679412+Not-Kyle@users.noreply.github.com>
2024-11-25 09:35:06 -0500

    Create screengui.lua

---
+++ keepmeinla/screengui.lua
@@ -0,0 +1,7 @@
+local coreGui = game:GetService'CoreGui';
+
+local hellokittysouljia = Instance.new('ScreenGui', coreGui)
+hellokittysouljia.Name = 'hellokittysouljia'
+hellokittysouljia.ResetOnSpawn = false
+hellokittysouljia.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+hellokittysouljia.IgnoreGuiInset = true
