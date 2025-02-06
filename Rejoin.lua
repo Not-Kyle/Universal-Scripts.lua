9bd96f5021da43b235d3a1474e680d87c1608851
kori <77679412+Not-Kyle@users.noreply.github.com>
2025-02-06 07:31:18 -0800

    Update Rejoin.lua

--- Rejoin.lua
+++ Rejoin.lua
@@ -1,13 +1,13 @@
-getgenv().TeleportService = game:GetService'TeleportService';
-getgenv().ContextService = game:GetService'ContextActionService';
-getgenv().Place = game.PlaceId;
+local TeleportService = game.GetService(game, 'TeleportService');
+local ContextService = game.GetService(game, 'ContextActionService');
+local PlaceId, JobId = game.PlaceId, game.JobId;
 
 local RejoinKey = 'F1'; -- Change your keybind here
 
 function Rejoin(Type:string, Properties:EnumItem)
     if Type == 'Rejoins' then
         if not Properties or Properties == Enum.UserInputState.Begin then
-            TeleportService:Teleport(Place)
+            TeleportService:TeleportToPlaceInstance(PlaceId, JobId)
         end
     end
 end
