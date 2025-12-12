f5a158e087628b755d6dfd529f37031c232dadd6
kori <77679412+Not-Kyle@users.noreply.github.com>
2025-12-12 00:23:58 -0800

    Adjust UI properties in Silent Aim.lua

--- UI/Silent Aim.lua
+++ UI/Silent Aim.lua
@@ -245,8 +245,8 @@ function Window:CreateWindow(WindowTitle: string)
                 BackgroundTransparency = 1,
                 BackgroundColor3 = Color3.fromRGB(255, 255, 255),
                 BorderColor3 = Color3.fromRGB(0, 0, 0),
-                Position = UDim2.new(0, 0, 1.02777779, 0),
-                Size = UDim2.new(0, 0, 0, Size),
+                BorderSizePixel = 0,
+                Size = UDim2.new(0, 100, 0, Size),
             })
         end
 
