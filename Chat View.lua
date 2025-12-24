286217109d8956346d413474df03ca7978e0125b
kori <77679412+Not-Kyle@users.noreply.github.com>
2025-12-24 00:41:53 -0800

    Create Chat View.lua

---
+++ Chat View.lua
@@ -0,0 +1,9 @@
+local TextChatService = game:GetService('TextChatService');
+local ChatWindowConfiguration = TextChatService and TextChatService:FindFirstChild('ChatWindowConfiguration');
+
+if not ChatWindowConfiguration then
+    return
+end
+
+ChatWindowConfiguration.Enabled = false;
+
