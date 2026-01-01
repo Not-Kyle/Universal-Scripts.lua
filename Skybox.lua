7718ab5e26ba1deb150f601455afef6f00ef64b0
kori <77679412+Not-Kyle@users.noreply.github.com>
2026-01-01 06:06:16 -0800

    Update Skybox.lua

--- Skybox.lua
+++ Skybox.lua
@@ -1,38 +1,13 @@
-getgenv().Lighting = game:GetService'Lighting'
-getgenv().RunService = game:GetService'RunService'
+local Lighting = game:GetService('Lighting');
 
-local ColorCorrection = false
-local Correction = false
-local SunRays = false
--- Change it to On and Off (true & false)
-Skybox = Instance.new("Sky", Lighting)
-Skybox.SkyboxBk = "rbxassetid://92464172"
-Skybox.SkyboxDn = "rbxassetid://92464250"
-Skybox.SkyboxFt = "rbxassetid://92464217"
-Skybox.SkyboxLf = "rbxassetid://92464234"
-Skybox.SkyboxRt = "rbxassetid://92464189"
-Skybox.SkyboxUp = "rbxassetid://92464157"
+Lighting:FindFirstChild('ColorCorrection').Parent = nil;
+Lighting:FindFirstChild('Correction').Parent = nil;
+Lighting:FindFirstChild('SunRaysEffect').Parent = nil;
 
-RunService.Stepped:Connect(function()
-   if Lighting then
-      if Lighting:FindFirstChild"ColorCorrection" then
-         if not ColorCorrection then
-            Lighting:WaitForChild"ColorCorrection":Destroy()
-         else
-            return nil
-         end
-      elseif Lighting:FindFirstChild"Correction" then
-         if not Correction then
-            Lighting:WaitForChild"Correction":Destroy()
-         else
-            return nil
-         end
-      elseif Lighting:FindFirstChildOfClass"SunRaysEffect" then
-         if not SunRays then
-            Lighting:WaitForChild"SunRaysEffect":Destroy()
-         else
-            return nil
-         end
-      end
-   end
-end)
+local Skybox = Instance.new('Sky', Lighting);
+Skybox.SkyboxBk = 'rbxassetid://92464172';
+Skybox.SkyboxDn = 'rbxassetid://92464250';
+Skybox.SkyboxFt = 'rbxassetid://92464217';
+Skybox.SkyboxLf = 'rbxassetid://92464234';
+Skybox.SkyboxRt = 'rbxassetid://92464189';
+Skybox.SkyboxUp = 'rbxassetid://92464157';
