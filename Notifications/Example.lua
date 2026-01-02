c8afcf5f02e28eacf641d46034d8336fb6a0d7f8
kori <77679412+Not-Kyle@users.noreply.github.com>
2026-01-01 18:41:11 -0800

    Add notification functionality in Example.lua

---
+++ Notifications/Example.lua
@@ -0,0 +1,8 @@
+local Import = loadstring(game:HttpGet('https://raw.githubusercontent.com/Not-Kyle/Universal-Scripts.lua/refs/heads/main/Notifications/Source.lua'))();
+local Window = Import:Panel(--[[ScreenGui]]); -- Insert your own screengui or leave it blank, there is one already defaulting to Panel
+
+function Notify(Text: string, Delay: number)
+    return Window:Notify(Text, Delay); -- Delay defaults to 5 if no time was set
+end
+
+Notify('TESTINGGGGG, NOTIFICATIONS BY MAWBORNE');
