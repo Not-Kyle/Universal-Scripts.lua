3aad08f25daffc328934c094c8086037f080e41f
kori <77679412+Not-Kyle@users.noreply.github.com>
2026-01-02 20:06:21 -0800

    Update Noclip.lua

--- Noclip.lua
+++ Noclip.lua
@@ -1,35 +1,72 @@
-getgenv().Players = game:GetService'Players'
-getgenv().Host = Players.LocalPlayer
-getgenv().AddNotification = function(title, text) game:GetService'StarterGui':SetCore("SendNotification", {Title = title; Text = text;}) end
-getgenv().RunService = game:GetService'RunService';
-getgenv().ContextActionService = game:GetService'ContextActionService';
+--[[
+How to use
+
+Press X on your keyboard
+]]--
+
+local Import = setmetatable({}, {
+    __index = function(self: Instance, ...)
+        local Arguments = {...}
+        rawset(self, Arguments, Arguments[1])
+        
+        return game:GetService(Arguments[1]);
+    end
+})
+
+local Players = Import.Players;
+local RunService = Import.RunService;
+local ContextActionService = Import.ContextActionService;
+
+local Host = Players and Players.LocalPlayer;
+local Body = Host and (Host.Character or Host.CharacterAdded:Wait())
 
 local Noclip = true;
-local NoclipKey = 'X'; -- Change your key here
+local Keybind = 'X';
 
-RunService.RenderStepped:Connect(function()
-    if Noclip then
-        for _,v in pairs(Host.Character:GetDescendants()) do
-            if v:IsA'BasePart' and v.CanCollide then
-                v.CanCollide = false
-            end
-        end
-    else
-        for _,v in pairs(Host.Character:GetDescendants()) do
-            if v:IsA'BasePart' and not v.CanCollide then
-                v.CanCollide = true
-            end
-        end
-    end
-end)
+local IndexedParts = {};
 
-function Noclipping(ActionName:string, Properties:EnumItem)
-    if ActionName == 'Noclip' then
-        if not Properties or Properties == Enum.UserInputState.Begin then
-            Noclip = not Noclip
-            AddNotification('Noclip','Noclip is now - '..tostring(Noclip))
+local Menu = loadstring(game:HttpGet('https://raw.githubusercontent.com/Not-Kyle/Universal-Scripts.lua/refs/heads/main/Notifications/Source.lua'))();
+local Window = Menu:Panel();
+
+function Notify(Text: string, Delay: number)
+    return Window:Notify(Text, Delay);
+end
+
+function GrabPart()
+    for _, Index in next, Body:GetDescendants() do
+        if Index:IsA('BasePart') then
+            table.insert(IndexedParts, Index);
         end
     end
 end
 
-ContextActionService:BindAction('Noclip', Noclipping, true, Enum.KeyCode[NoclipKey])
+function OnHeartbeat(Delta: number)
+    for _, Index in ipairs(IndexedParts) do
+        Index.CanCollide = not Noclip
+    end
+end
+
+function OnCharacterAdded(Character: Model)
+    Body = Character or Host.Character;
+end
+
+function ActionKey(ActionName:string, Properties:EnumItem)
+    if ActionName == 'Noclip' then
+        if not Properties or Properties == Enum.UserInputState.Begin then
+            Noclip = not Noclip
+
+            GrabPart();
+            Notify(string.format('Noclip is now: %s', tostring(Noclip) or 'null'))
+        end
+    end
+end
+
+function CheatData()
+    Host.CharacterAdded:Connect(OnCharacterAdded);
+    RunService.Heartbeat:Connect(OnHeartbeat);
+
+    ContextActionService:BindAction('Noclip', ActionKey, true, Enum.KeyCode[Keybind])
+
+    Notify(string.format('Mawborn Noclip V2 %s', 'loaded'))
+end
+CheatData();
